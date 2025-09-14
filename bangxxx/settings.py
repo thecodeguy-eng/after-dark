7036b0a7fa951b2ee52e2474d6b3898970b4c56c
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2025-09-14 05:21:39 +0100

    static

--- bangxxx/settings.py
+++ bangxxx/settings.py
@@ -99,9 +99,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Media files
 MEDIA_URL = '/media/'
